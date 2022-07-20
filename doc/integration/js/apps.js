a484a71106ffd5e76963ef68985e0423cbca8116
LucasMachut <machut.lucas@gmail.com>
2022-07-20 15:14:33 +0200

    cleanig up index

--- doc/integration/js/apps.js
+++
@@ -1,7 +0,0 @@
-let container = document.getElementById('container');
-window.onmousemove = function(e) {
-    let x = - e.clientX/5,
-        y = - e.clientY/5;
-        container.style.backgroundPositionX = x +'px';
-        container.style.backgroundPositionY = y +'px';
-}
