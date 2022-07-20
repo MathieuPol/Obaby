ff85a7d6d09c3748bff4c271ebe6d58c383342f5
Julie-QUIVRON <julie.quivron.pro@gmail.com>
2022-07-20 13:48:34 +0200

    ci:done CategoryController

--- src/Controller/Front/CategoryController.php
+++ src/Controller/Front/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/front/category", name="app_front_category")
+     * @Route("/category/question", name="category_show_question")
      */
-    public function index(): Response
+    public function showQuestion(CategoryRepository $categoryRepository): Response
     {
-        return $this->render('front/category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+        return $this->render('Front/category/questionList.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/category/practice", name="category_show_practice")
+     */
+    public function showPtractice(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('Front/category/practiceList.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
