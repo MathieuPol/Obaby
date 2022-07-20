daefae9796cf3f317b067670a667b2b4e1ae293a
yoann-chaperon <yochaps@gmail.com>
2022-07-20 10:18:04 +0200

    create barnch upper slider

--- doc/integration/js/main.js
+++ doc/integration/js/main.js
@@ -6,24 +6,24 @@
 
 
     // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+    // var spinner = function () {
+    //     setTimeout(function () {
+    //         if ($('#spinner').length > 0) {
+    //             $('#spinner').removeClass('show');
+    //         }
+    //     }, 1);
+    // };
+    // spinner();
 
 
     // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 300) {
+    //         $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    //     } else {
+    //         $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+    //     }
+    // });
     
     
     // Back to top button
