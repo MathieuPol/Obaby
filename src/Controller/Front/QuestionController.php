a1d0b22ed8d1748df024993421909dbcb75dc1b5
Julie-QUIVRON <julie.quivron.pro@gmail.com>
2022-07-20 13:36:08 +0200

    ci:done method ask and rename templates

--- src/Controller/Front/QuestionController.php
+++ src/Controller/Front/QuestionController.php
@@ -10,6 +10,7 @@ use App\Entity\Question;
 use App\Form\AnswerType;
 use App\Repository\AnswerRepository;
 use App\Entity\Answer;
+use App\Repository\QuestionRepository;
 use Symfony\Component\HttpFoundation\Request;
 
 class QuestionController extends AbstractController
@@ -37,8 +38,6 @@ class QuestionController extends AbstractController
         $answer = new Answer();
         $form = $this->createForm(AnswerType::class, $answer);
 
-        
-
         return $this->render('front/question/show.html.twig', [
             'questions' => $questions,
             'category' => $category,
@@ -49,27 +48,46 @@ class QuestionController extends AbstractController
     /**
      * @Route("/question/{id}/answer", name="question_answer", methods={"POST"})
      */
-     public function answer(Question $question, Request $request): Response
-     {
-         $answer = new Answer();
-         $form = $this->createForm(AnswerType::class, $answer);
-         $form->handleRequest($request);
- 
-         if ($form->isSubmitted() && $form->isValid()) {
-             $answer->setQuestion($question);
-             $this->getDoctrine()->getManager()->persist($answer);
-             $this->getDoctrine()->getManager()->flush();
- 
-             return $this->redirectToRoute('question_list', [
-                 'id' => $question->getCategory()->getId()
-             ]);
-         }
- 
-         return $this->renderForm('front/question/answer.html.twig', [
-             'question' => $question,
-             'form' => $form->createView()
-         ]);
-     }
+    public function answer(Question $question, Request $request, AnswerRepository $answerRepository): Response
+    {
+        $answer = new Answer();
+        $form = $this->createForm(AnswerType::class, $answer);
+        $form->handleRequest($request);
 
-    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $answer->setQuestion($question);
+
+            $answerRepository->add($answer, true);
+
+            return $this->redirectToRoute('question_list', [
+                'id' => $question->getCategory()->getId()
+            ]);
+        }
+
+        return $this->renderForm('front/question/answer.html.twig', [
+            'question' => $question,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/ask", name="question_ask", methods={"GET", "POST"})
+     */
+    public function ask(Request $request, QuestionRepository $questionRepository): Response
+    {
+        $question = new Question();
+        $form = $this->createForm(QuestionType::class, $question);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $questionRepository->add($question, true);
+
+            return $this->redirectToRoute('app_question_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('Front/question/ask.html.twig', [
+            'question' => $question,
+            'form' => $form,
+        ]);
+    }
 }
