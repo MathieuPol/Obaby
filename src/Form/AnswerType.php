34aa0de7c0e73a4f4b7170b4e48c8e4ae9a434ff
LucasMachut <machut.lucas@gmail.com>
2022-07-21 14:36:49 +0200

    ci : done create practice and category forms

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -1,21 +1,26 @@
 <?php
 
+// Form for front answers
+
 namespace App\Form;
 
 use App\Entity\Answer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class AnswerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-            ->add('createdAt')
-            ->add('status')
-        ;
+            ->add('content', TextType::class, [
+            'label' => 'Rédigez votre réponse',
+            'help' => 'Ce champ ne doit pas être nul',
+            'constraints' => new NotBlank(),
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/PracticeType.php
+++ src/Form/PracticeType.php
@@ -4,20 +4,28 @@ namespace App\Form;
 
 use App\Entity\Practice;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
 
 class PracticeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('status')
-        ;
+            ->add('title', TextType::class, [
+            'label' => 'Donner un titre à votre article',
+            'help' => 'Ce champ ne doit pas être nul',
+            'constraints' => new NotBlank(),
+            ])
+            ->add('content', TextType::class, [
+            'label' => 'Rédigez votre article',
+            'help' => 'Ce champ ne doit pas être nul',
+            'constraints' => new NotBlank(),
+            ])
+    ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
