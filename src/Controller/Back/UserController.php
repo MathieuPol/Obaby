d099e0a7e5281922086c0a9bc7f42b2e29a3a5e7
Mathieu Poltorak <99071788+MathieuPol@users.noreply.github.com>
2022-07-23 18:18:37 +0200

    Update UserController.php

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -26,7 +26,7 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="back_user_show", methods={"GET"})
+     * @Route("/{id}", name="show", methods={"GET"})
      */
     public function show(User $user): Response
     {
