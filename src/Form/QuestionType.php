07c52d14917f20262ff896bf06352deff0f76e3e
yoann-chaperon <yochaps@gmail.com>
2022-07-26 10:49:58 +0200

    edit TextareaType in QuestionType

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -6,6 +6,7 @@ namespace App\Form;
 use App\Entity\Question;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,7 @@ class QuestionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content', TextType::class, [
+            ->add('content', TextareaType::class, [
                 'label' => 'Posez votre question',
                 'help' => 'Ce champ ne doit pas être nul',
                 'constraints' => new NotBlank(),
