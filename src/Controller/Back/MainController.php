f73bd27f6c76ebbc5c08d504202e189bdf89ba3e
Julie-QUIVRON <julie.quivron.pro@gmail.com>
2022-07-28 16:07:10 +0200

    build : tableau category in backhome

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Back;
 
 use App\Entity\User;
 use App\Repository\AnswerRepository;
+use App\Repository\CategoryRepository;
 use App\Repository\PracticeRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\UserRepository;
@@ -25,18 +26,21 @@ class MainController extends AbstractController
     public function home(UserRepository $userRepository,
                         PracticeRepository $practiceRepository,
                         QuestionRepository $questionRepository,
-                        AnswerRepository $answerRepository ): Response
+                        AnswerRepository $answerRepository,
+                        CategoryRepository $categoryRepository): Response
     {
         $users = $userRepository->findBy([], ['id' => 'DESC'], 5);
         $practices = $practiceRepository->findBy([], ['createdAt' => 'DESC'], 5);
         $questions = $questionRepository->findBy(array(), array('createdAt' => 'DESC'), 5);
         $answers = $answerRepository->findBy(array(), array('createdAt' => 'Desc'),5);
+        $categories = $categoryRepository->findAll();
 
         return $this->render('Back/home.html.twig', [
             'users' => $users,
             'practices' => $practices,
             'questions' => $questions,
             'answers' => $answers,
+            'categories'=> $categories,
         ]);
     }
 }
