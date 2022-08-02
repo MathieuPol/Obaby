07202d07bacf322a5a8d4b6b9f9659b08483f017
Julie-QUIVRON <julie.quivron.pro@gmail.com>
2022-08-02 09:55:58 +0200

    refactor:done back edit user page

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,21 +14,26 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',TextType::class, [
+                'label' => '* Email',
+                ])
             ->add('roles', ChoiceType::class,
-            [
+            [   'label' => '* Rôles',
+            'help' => '* Champs obligatoires',
                 'choices' => [
-                    'user' => 'ROLE_USER',
-                    'admin' => 'ROLE_ADMIN',
+                    'utilisateur' => 'ROLE_USER',
+                    'administrateur' => 'ROLE_ADMIN',
                     'modérateur' => 'ROLE_MODERATOR',
                 ],
                 "multiple" => true,
                 // radio buttons or checkboxes
                 "expanded" => true,
             ])
-            ->add('pseudo')
+            ->add('pseudo',TextType::class, [
+                'label' => '* Pseudonyme',
+                ])
             ->add('status', ChoiceType::class,
-            [
+            [   'label' => '* Statut',
                 'choices' => [
                     'activé' => '1',
                     'désactivé' => '0',            
