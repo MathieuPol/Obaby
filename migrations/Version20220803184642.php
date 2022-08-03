6984d0219dcef9013852b99947bf0d357b45fcbf
MathieuPol <poltorakmathieu@gmail.com>
2022-08-03 21:20:12 +0200

    ci: done practice banner

--- migrations/Version20220803184642.php
+++ migrations/Version20220803184642.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220803144633 extends AbstractMigration
+final class Version20220803184642 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,6 +21,7 @@ final class Version20220803144633 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE answer (id INT AUTO_INCREMENT NOT NULL, question_id INT NOT NULL, user_id INT DEFAULT NULL, content LONGTEXT NOT NULL, created_at DATE NOT NULL, status SMALLINT NOT NULL, INDEX IDX_DADD4A251E27F6BF (question_id), INDEX IDX_DADD4A25A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(64) NOT NULL, slug VARCHAR(64) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE practice (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, user_id INT DEFAULT NULL, title VARCHAR(127) NOT NULL, content LONGTEXT NOT NULL, created_at DATE NOT NULL, updated_at DATE DEFAULT NULL, status SMALLINT NOT NULL, slug VARCHAR(255) NOT NULL, INDEX IDX_7FEC344E12469DE2 (category_id), INDEX IDX_7FEC344EA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, user_id INT DEFAULT NULL, content LONGTEXT NOT NULL, created_at DATE NOT NULL, status SMALLINT NOT NULL, INDEX IDX_B6F7494E12469DE2 (category_id), INDEX IDX_B6F7494EA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(64) NOT NULL, status SMALLINT NOT NULL, slug VARCHAR(64) NOT NULL, genre VARCHAR(50) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D64986CC499D (pseudo), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -36,11 +37,14 @@ final class Version20220803144633 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE practice DROP FOREIGN KEY FK_7FEC344E12469DE2');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E12469DE2');
         $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A251E27F6BF');
         $this->addSql('ALTER TABLE answer DROP FOREIGN KEY FK_DADD4A25A76ED395');
         $this->addSql('ALTER TABLE practice DROP FOREIGN KEY FK_7FEC344EA76ED395');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494EA76ED395');
         $this->addSql('DROP TABLE answer');
+        $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE practice');
         $this->addSql('DROP TABLE question');
         $this->addSql('DROP TABLE user');
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -107,35 +107,36 @@ class AppFixtures extends Fixture
 
 
         // Category creation
-
-            $category1 = new Category();
-            $category1->setName('Grossesse');
-            $categorySlug1 = $this->slug->slug($category1->getName())->lower();
-            $category1->setSlug($categorySlug1);
-            $manager->persist($category1);
-            $categoryList[] = $category1;
-
-            $category2 = new Category();
-            $category2->setName('Sécurité');
-            $categorySlug2 = $this->slug->slug($category2->getName())->lower();
-            $category2->setSlug($categorySlug2);
-            $manager->persist($category2);
-            $categoryList[] = $category2;
-
-            $category3 = new Category();
-            $category3->setName('Nutrition');
-            $categorySlug3 = $this->slug->slug($category3->getName())->lower();
-            $category3->setSlug($categorySlug3);
-            $manager->persist($category3);
-            $categoryList[] = $category3;
-
-            $category4 = new Category();
+        
+        
+        $category1 = new Category();
+        $category1->setName('Grossesse');
+        $categorySlug1 = $this->slug->slug($category1->getName())->lower();
+        $category1->setSlug($categorySlug1);
+        $manager->persist($category1);
+        $categoryList[] = $category1;
+        
+        $category2 = new Category();
+        $category2->setName('Sécurité');
+        $categorySlug2 = $this->slug->slug($category2->getName())->lower();
+        $category2->setSlug($categorySlug2);
+        $manager->persist($category2);
+        $categoryList[] = $category2;
+        
+        $category3 = new Category();
+        $category3->setName('Nutrition');
+        $categorySlug3 = $this->slug->slug($category3->getName())->lower();
+        $category3->setSlug($categorySlug3);
+        $manager->persist($category3);
+        $categoryList[] = $category3;
+        
+        $category4 = new Category();
             $category4->setName('Santé et soins');
             $categorySlug4 = $this->slug->slug($category4->getName())->lower();
             $category4->setSlug($categorySlug4);
             $manager->persist($category4);
             $categoryList[] = $category4;
-
+            
             $category5 = new Category();
             $category5->setName('Loisir');
             $categorySlug5= $this->slug->slug($category5->getName())->lower();
@@ -150,43 +151,49 @@ class AppFixtures extends Fixture
             $manager->persist($category6);
             $categoryList[] = $category6;
             
-
-
-
-        // Question creation
-
-        for ($j = 0; $j < 30; $j++) {
-            $question = new Question();
-            $question->setContent($faker->sentence(true));
-            $question->setCategory($categoryList[array_rand($categoryList)]);
-            $date = $faker->date('Y-m-d');
-            $question->setCreatedAt(new \DateTime($date));
-            $question->setStatus($faker->numberBetween(0, 1));
-            $question->setUser($userList[array_rand($userList)]);
-
-            $manager->persist($question);
-            $questionList[] = $question;
-        }
-
-        // Answer creation
-
-        for ($k = 0; $k < 30; $k++) {
-            $answer = new Answer();
-            $answer->setContent($faker->paragraph(true));
-            $dateAnswer = $faker->date('Y-m-d');
-            $answer->setCreatedAt(new \DateTime($dateAnswer));
-            $answer->setQuestion($questionList[array_rand($questionList)]);
-            $answer->setStatus($faker->numberBetween(0, 1));
-            $answer->setUser($userList[array_rand($userList)]);
-
-            $manager->persist($answer);
-            $answerList[] = $answer;
-        }
-
-        // Practice creation
-
-        for($l = 0; $l < 30; $l++) {
+            
+            
+            
+            // Question creation
+            
+            for ($j = 0; $j < 30; $j++) {
+                $question = new Question();
+                $question->setContent($faker->sentence(true));
+                $question->setCategory($categoryList[array_rand($categoryList)]);
+                $date = $faker->date('Y-m-d');
+                $question->setCreatedAt(new \DateTime($date));
+                $question->setStatus($faker->numberBetween(0, 1));
+                $question->setUser($userList[array_rand($userList)]);
+                
+                $manager->persist($question);
+                $questionList[] = $question;
+            }
+            
+            // Answer creation
+            
+            for ($k = 0; $k < 30; $k++) {
+                $answer = new Answer();
+                $answer->setContent($faker->paragraph(true));
+                $dateAnswer = $faker->date('Y-m-d');
+                $answer->setCreatedAt(new \DateTime($dateAnswer));
+                $answer->setQuestion($questionList[array_rand($questionList)]);
+                $answer->setStatus($faker->numberBetween(0, 1));
+                $answer->setUser($userList[array_rand($userList)]);
+                
+                $manager->persist($answer);
+                $answerList[] = $answer;
+            }
+            
+            // Practice creation
+            $pictureList = [
+                'enfant-1400.jpg',
+                'ecrire-1400.png',
+                'phone-1400.png',
+            ];
+            
+            for($l = 0; $l < 30; $l++) {
             $practice = new Practice();
+            $practice->setPicture($pictureList[array_rand($pictureList)]);
             $practice->setTitle($faker->sentence(true));
             $practice->setSlug($this->slugService->slug($practice->getTitle()));
             $practice->setContent($faker->paragraph(3));
