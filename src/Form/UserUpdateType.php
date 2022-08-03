49e7aa0d131c49520275a4095cb5eb6cd9cca7c6
Julie-QUIVRON <julie.quivron.pro@gmail.com>
2022-08-03 11:03:16 +0200

    refactor: done page edit my account

--- src/Form/UserUpdateType.php
+++ src/Form/UserUpdateType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,8 +18,12 @@ class UserUpdateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('pseudo')
+            ->add('email', TextType::class, [
+                'label' => '* Email',
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => '* Pseudonyme',
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les mots de passe doivent correspondre',
